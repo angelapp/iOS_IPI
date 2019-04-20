4bd47e7a3d9d5f152463d7bd82272b1c05ad8174
Felipe Zamudio <okazakitomoyo729@gmail.com>
2019-04-20 18:49:10 -0500

    Create IPI Project and File System.

---
+++ IPI/IPI/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IPI
+//
+//  Created by Felipe Zamudio on 20/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
