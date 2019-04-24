226a7128a79e23e9135cba08bbd53f7175256dc2
Felipe Zamudio <okazakitomoyo729@gmail.com>
2019-04-23 20:32:02 -0500

    add colors

---
+++ IPI/IPI/Resourses/Colors/Colors.swift
@@ -0,0 +1,38 @@
+//
+//  Colors.swift
+//  IPI
+//
+//  Created by Felipe Zamudio on 20/04/19.
+//  Copyright © 2019 NRC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Colors: NSObject {
+    
+    /// Optiene el color desde su referencia HEX
+    /// - Parameter from: Hexa del color
+    /// - Returns: UIColor correspondiente al hexa de entrada
+    func getColor (from hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
---
+++ IPI/IPI/Resourses/Colors/IPIColors.swift
@@ -0,0 +1,39 @@
+//
+//  IPIColors.swift
+//  IPI
+//
+//  Created by Felipe Zamudio on 20/04/19.
+//  Copyright © 2019 NRC. All rights reserved.
+//
+
+import Foundation
+
+/// Listado de los colores usados en formado HEX
+enum ConseColors: String{
+    
+    case background_blue = "00CFD3"
+    case background_gray = "F7F7F7"
+    case background_acordeon = "E6E6E5"
+    
+    case button_blue = "008690"
+    case button_facebook = "3B5998"
+    
+    case progress_green = "0ef90a"
+    
+    case texts = "00455C"
+    case texts_blue = "004058"
+    
+    case black = "000000"
+    case blue_light = "C7EDEC"
+    case gray_dark = "95989A"
+    case gray_hard = "333333"
+    case gray_spinner = "81a1ab"
+    case orange = "FF7602"
+    case pink_light = "F8D4D5"
+    case salmon = "eb5c3f"
+    case white = "FFFFFF"
+    case yellow = "F9EBB8"
+    case yellow_dark = "FFDE4E"
+    
+    case colorTrans = "80000000"
+}
